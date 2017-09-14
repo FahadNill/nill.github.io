a775a09d6cb50131cb6e41b1e1fc9750fd621975
FahadNill <30323647+FahadNill@users.noreply.github.com>
2017-09-14 18:44:20 +0600

    section-15

---
+++ RRF/100_Section/15 - Jquery Masonry with Isotope/assets/js/main.js
@@ -0,0 +1,24 @@
+jQuery(document).ready(function($){
+
+	$(".portfolio-menu li").on('click', function() {
+
+		var filterData = $(this).attr("data-filter");
+
+		$(".portfolio-list").isotope({
+			filter: filterData,
+		});
+
+		$(".portfolio-menu li").removeClass('active');
+		$(this).addClass('active');
+
+	});
+
+	$(".portfolio-list").isotope({
+		itemSelector: '.single-portfolio',
+		percentPosition: true,
+		masonry: {
+			columnwidth: '.single-portfolio',
+			horizontalOrder: true
+		}
+	});
+});
