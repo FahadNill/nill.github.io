d75169022708555bd7cd9b60030c9750937d2d8e
FahadNill <30323647+FahadNill@users.noreply.github.com>
2017-09-20 00:28:58 +0600

    twist

--- RRF/Psd To Html/Twist/assets/js/script.js
+++ RRF/Psd To Html/Twist/assets/js/script.js
@@ -7,4 +7,29 @@ jQuery(document).ready(function($){
 		navText: ["<i class='fa fa-long-arrow-left'></i>", "<i class='fa fa-long-arrow-right'></i>"],
 		autoplay: true
 	});
+
+	$(".circle").circleProgress({
+		value: 0.9,
+		size: 260,
+		fill: '#088E93',
+		thickness: 3
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.web-circle').html(Math.round(90 * progress) + '<i>%</i>');
+  });
+	$(".circle1").circleProgress({
+		value: 0.95,
+		size: 260,
+		fill: '#088E93',
+		thickness: 3
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.graphic-circle').html(Math.round(95 * progress) + '<i>%</i>');
+  });
+	$(".circle2").circleProgress({
+		value: 0.84,
+		size: 260,
+		fill: '#088E93',
+		thickness: 3
+	}).on('circle-animation-progress', function(event, progress) {
+    $(this).find('.marketing-circle').html(Math.round(84 * progress) + '<i>%</i>');
+  });
 });	
