ec902ee0b3575b8fe27a9b64fdb52487bf78f935
FahadNill <30323647+FahadNill@users.noreply.github.com>
2017-10-12 19:38:50 +0600

    full width carousel section

---
+++ RRF/100-Section/Full width Carousel Section/assets/js/script.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+    $(".projects-list").owlCarousel({
+        items: 5,
+        loop: true,
+        nav: false,
+        dots: true,
+        autoplay: true
+    })
+})	
