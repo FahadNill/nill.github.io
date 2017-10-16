6c0380aba51d8a88ab5cc0f71649fedd5b552ced
FahadNill <30323647+FahadNill@users.noreply.github.com>
2017-10-17 03:43:18 +0600

    landing page

--- RRF/100-Section/Landing Page/assets/js/script.js
+++ RRF/100-Section/Landing Page/assets/js/script.js
@@ -1,5 +1,25 @@
 jQuery(document).ready(function($){
     $(".portfolio-list").masonry({
         
+    });
+
+    $(".single-item").hover(function(){
+    	$(".single-img-hover h4, .single-img-hover p").removeClass("animated fadeInUp")
+    	$(this).find(".single-img-hover h4, .single-img-hover p").addClass("animated fadeInUp")
+    })
+
+    $(".offcanvas-menu ul li").on('click', function(){
+    	$(".offcanvas-menu ul li").removeClass("active")
+    	$(this).addClass("active")
+    })
+
+    $(".menu-collapse").on('click', function(){
+    	$(".offcanvas").addClass("active2")
+    	$(".offcanvas-overlay").addClass("active")
+    })
+
+    $(".close-btn, .offcanvas-overlay").on('click', function(){
+    	$(".offcanvas").removeClass("active2")
+    	$(".offcanvas-overlay").removeClass("active")
     })
 })	
