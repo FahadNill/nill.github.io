ef3812744d1f714df3ab3ae1fd8d1ec3a8380632
FahadNill <30323647+FahadNill@users.noreply.github.com>
2017-12-04 13:53:52 +0600

    Edutune

---
+++ Edutune/assets/js/script.js
@@ -0,0 +1,28 @@
+(function ($) {
+	"use strict";
+
+    jQuery(document).ready(function($){
+
+    	$(".slider_area").owlCarousel({
+    		loop: true,
+    		items: 1,
+    		nav: false,
+    		dots: false,
+    		autoplay: false
+    	});
+
+
+
+        
+
+
+    });
+
+
+    jQuery(window).load(function(){
+
+        
+    });
+
+
+}(jQuery));	
